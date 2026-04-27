b6e0b9679836f4cbfe44b779f833bb79762de2fa
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-04-27 10:43:36 +0200

    VaultDetailPage exported from the detail barrel

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,3 +2,5 @@ export { useNavigation   } from "./useNavigation";
 export { useVaultFilters } from "./useVaultFilters";
 export { useDepositForm  } from "./useDepositForm";
 export { useHover        } from "./useHover";
+export { useVault        } from "./useVault";
+export type { UseVaultReturn, OnChainVaultState } from "./useVault";
